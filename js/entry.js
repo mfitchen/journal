65fcd91f2003e030449d14027117c535a231a51e
Marty Fitchen and Noah Yasskin <noah@>
2016-08-01 16:33:44 -0700

    add working single js file with both front end and back end together

--- js/entry.js
+++ js/entry.js
@@ -8,7 +8,7 @@ Entry.prototype.words = function(bodyEntry) {
 }
 
 Entry.prototype.vowels = function(bodyEntry) {
-  bodyEntry = bodyEntry.replaceAll("[^A-Za-z]+", "").toLowerCase();
+  bodyEntry = bodyEntry.toLowerCase();
   var vowelsCount = 0;
   for (var i = 0; i <= bodyEntry.length; i += 1) {
     if (bodyEntry.charAt(i) == "a" || bodyEntry.charAt(i) == "e" || bodyEntry.charAt(i) == "i" || bodyEntry.charAt(i) == "o" || bodyEntry.charAt(i) == "u") {
@@ -19,7 +19,7 @@ Entry.prototype.vowels = function(bodyEntry) {
 }
 
 Entry.prototype.consonants = function(bodyEntry) {
-  bodyEntry = bodyEntry.replaceAll("[^A-Za-z]+", "").toLowerCase();
+  bodyEntry = bodyEntry.toLowerCase();
   var consonantsCount = 0;
   for (var i = 0; i <= bodyEntry.length; i += 1) {
     if (bodyEntry.charAt(i) == "a" || bodyEntry.charAt(i) == "e" || bodyEntry.charAt(i) == "i" || bodyEntry.charAt(i) == "o" || bodyEntry.charAt(i) == "u") {
@@ -30,6 +30,23 @@ Entry.prototype.consonants = function(bodyEntry) {
   }
   return consonantsCount;
 }
+
+// exports.entryModule = Entry;
+
+$(document).ready(function() {
+  $('#entry-form').submit(function(event) {
+    event.preventDefault();
+    var titleEntry = $('#title-entry').val();
+    var bodyEntry = $('#body-entry').val();
+    var newEntry = new Entry(titleEntry, bodyEntry);
+    var words = newEntry.words(bodyEntry);
+    var vowels = newEntry.vowels(bodyEntry);
+    var consonants = newEntry.consonants(bodyEntry);
+    $('#number-words').text(words);
+    $('#number-vowels').text(vowels);
+    $('#number-consonants').text(consonants);
+  });
+});
 
 // methods for number of words, vowels, and consonants
 // method called getTeaser to return the first sentence of the entry (only the first 8 words)
